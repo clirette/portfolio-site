feabf8ce7f8b5f9808d045bc0247233b5f4391d6
Lirette <clirette@dxc.com>
2019-03-06 10:18:50 -0600

    Removed comments/logs

--- js/index.js
+++ js/index.js
@@ -4,12 +4,9 @@ const work = document.getElementById('work');
 const contact = document.getElementById('contact');
 
 const scrollToDiv = (e) => {
-  // console.log(`${e.target.innerText.toLowerCase()}`);
   const divToScrollTo = document.getElementById(e.target.innerText.toLowerCase());
   const y = divToScrollTo.offsetTop;
-  console.log(divToScrollTo);
   window.scrollTo(0, y);
-  // window.scrollTo = document.getElementById(`${e.target.innerText.toLowerCase()}`);
 }
 
 aboutLink.addEventListener('click', scrollToDiv);
