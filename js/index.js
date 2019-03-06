a28968e852350b9c071296b386cd7ec0e2bbccf4
Lirette <clirette@dxc.com>
2019-03-06 10:21:59 -0600

    Toggle current link on click.

--- js/index.js
+++ js/index.js
@@ -3,7 +3,11 @@ const about = document.getElementById('about');
 const work = document.getElementById('work');
 const contact = document.getElementById('contact');
 
+const links = [aboutLink, workLink, contactLink];
+
 const scrollToDiv = (e) => {
+  links.forEach(link => link.classList.remove('current'));
+  e.target.classList.add('current');
   const divToScrollTo = document.getElementById(e.target.innerText.toLowerCase());
   const y = divToScrollTo.offsetTop;
   window.scrollTo(0, y);
