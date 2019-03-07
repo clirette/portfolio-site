3a51352251681e2f0e3407460d7af1ac17a140ba
Lirette <clirette@dxc.com>
2019-03-07 10:50:22 -0600

    Maybe fix scrolling on mobile and refactor xs screens

--- js/index.js
+++ js/index.js
@@ -15,4 +15,16 @@ aboutLink.addEventListener('click', switchCurrentLink);
 workLink.addEventListener('click', switchCurrentLink);
 contactLink.addEventListener('click', switchCurrentLink);
 
+const mobileScroll = (e) => {
+  e.preventDefault();
+  links.forEach(link => link.classList.remove('current'));
+  e.target.classList.add('current');
+  const divToScrollTo = document.getElementById(e.target.href.split('#')[1]);
+  window.scrollTo(0, divToScrollTo.offsetTop);
+}
+
+aboutLink.addEventListener('touchstart', mobileScroll);
+workLink.addEventListener('touchstart', mobileScroll);
+contactLink.addEventListener('touchstart', mobileScroll);
+
 const scroll = new SmoothScroll('a[href*="#"]');
